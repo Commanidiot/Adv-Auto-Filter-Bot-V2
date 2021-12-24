cb2279703fea3f5ea81a8ba45f049871904f07d9
Murali <91236773+muralitheprogrammer@users.noreply.github.com>
2021-12-24 17:34:53 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,33 +7,28 @@ class Translation(object):
     START_TEXT = """<b>𝙔𝙊 𝙔𝙊 𝙂𝙤𝙤𝙙 𝙈𝙤𝙧𝙣𝙞𝙣𝙜 {} 𝙈𝙮 𝙁𝙧𝙞𝙚𝙣𝙙.𝙄𝙖𝙢 𝙥𝙤𝙬𝙚𝙧𝙛𝙪𝙡 𝙖𝙣𝙙 𝙬𝙤𝙧𝙠 𝙛𝙤𝙧 <b><i><a href="https://t.me/NTG_family">𝙈𝙪𝙧𝙖𝙡𝙞</a></i></b>!!</b> 𝙩𝙝𝙖𝙩 𝙢𝙚𝙖𝙣 𝙢𝙮 𝙖𝙙𝙢𝙞𝙣 
 𝙞 𝙛𝙤𝙧𝙜𝙤𝙩 𝙩𝙤 𝙞𝙣𝙩𝙧𝙤𝙙𝙪𝙘𝙚 𝙢𝙮 𝙬𝙤𝙧𝙠𝙞𝙣𝙜 𝙜𝙧𝙥 𝙞𝙨 <b><i><a href="https://t.me/T5links">𝙈𝙒 𝙒𝙊𝙍𝙇𝘿</a></i></b> 𝙞𝙛 𝙮𝙤𝙪𝙧 𝙣𝙤𝙩 𝙖 𝙢𝙚𝙢𝙗𝙚𝙧 𝙞𝙣 𝙤𝙪𝙧 𝙛𝙖𝙢𝙞𝙡𝙮 𝙜𝙤𝙝𝙚𝙖𝙙 𝙩𝙤 𝙗𝙚 𝙛𝙖𝙢𝙞𝙡𝙮
  <b><i><a href="https://t.me/NTG_family">𝙈𝙐𝙍𝘼𝙇𝙄</a></i></b>
-    help_TEXT = """
-<b><i><u>If Any Issues How to connect?</u></i></b>
+    Help_TEXT = """
+<b><i><u> 𝗢𝘂𝗿 𝗙𝗮𝗺𝗶𝗹𝘆 𝗥𝗼𝗼𝘁</u></i></b>
+𝗔𝗱𝗺𝗶𝗻 -<b><i><a href="https://t.me/NTG_family">𝗠𝘂𝗿𝗮𝗹𝗶</a></i></b>
+       
 
-<i>
-👉 Come Admin Or Owner 
-👉 Contact To @NTG_Family
-</i>
 
-<b>Bot How Be Safe From Illegal Persons:</b>
 
-    👉 If Any Persons Who Any Illegal Thing Don't Believe Them 
-      
-    👉 If Any Offer  U Got Get Confirmed  To Admin Or Owner 
-            
-@NTG_family
-"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> ℳøvЇᎬꢺ⚔️𝕃𝕆𝕍𝔼ℝ𝕊 ❤
- </code>
-    
-<b>➥Movie world Creator</b> : <b><i><a href="https://t.me/NTG_family">NTG_family</a></i></b>
-
-<b>➥ Language</b> : 🤏😎 முரளி 🤏😎
-
-<b>➥ Movies World</b> : @T5links
-
-<b> 😍 Movie World Bros
-
-"""
-
+    Status_TEXT = 
